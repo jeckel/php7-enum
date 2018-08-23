a910a5b629ab735cc3a1fc61deecf100313b29d4
Jeckel <jeckel@jeckel.fr>
2018-08-23 17:49:20 +0200

    Add PHP7 Enum

---
+++ src/EnumAbstract.php
@@ -0,0 +1,91 @@
+<?php
+namespace PHP7Enum;
+
+/**
+ * Class EnumAbstract
+ *
+ * This class allow to create Real Enum type, you can then easily validate that force a value to be in the required type
+ */
+class EnumAbstract
+{
+    /** @var array */
+    static protected $allowedValues = [];
+
+    /** @var array */
+    static protected $cache = [];
+
+    /** @var mixed */
+    protected $value;
+
+    /**
+     * EnumAbstract constructor.
+     * Prevent use of constructor, to create a value, see __callStatic
+     * @param mixed $value
+     */
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return array
+     * @throws \ReflectionException
+     */
+    static public function toArray(): array
+    {
+        $class = static::class;
+        if (empty(static::$allowedValues[$class])) {
+            $reflection = new \ReflectionClass($class);
+            static::$allowedValues[$class] = $reflection->getConstants();
+        }
+
+        return static::$allowedValues[$class];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->value;
+    }
+
+    /**
+     * This method is the only way to retrieve an Enum value
+     * Returns a value when called statically like so: MyEnum::SOME_VALUE() given SOME_VALUE is a class constant
+     * The object returned is then a valid instance of MyEnum, with the value of SOME_VALUE
+     *
+     * If you retrieve twice for the same value, it will returned the same instance, you can then compare them
+     *
+     * @param string $name
+     * @param array  $arguments
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    static public function __callStatic(string $name, array $arguments = [])
+    {
+        $array = static::toArray();
+        if (! isset($array[$name])) {
+            throw new UnexpectedValueException(sprintf(
+                'Invalid value "%s" for enum "%s"',
+                $name,
+                static::class
+            ));
+        }
+        $value = $array[$name];
+
+        if (! isset(static::$cache[$value])) {
+            static::$cache[$value] = new static($value);
+        }
+
+        return static::$cache[$value];
+    }
+}
---
+++ src/UnexpectedValueException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PHP7Enum;
+
+/**
+ * Class InvalidEnumValue
+ * @package PHP7Enum
+ */
+class UnexpectedValueException extends \InvalidArgumentException
+{
+
+}
